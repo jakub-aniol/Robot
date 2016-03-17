8cfdd66f628d81f10e0199dac8e69b0ad4fb415c
Jakub Aniol <jakub_aniol@epam.com>
2016-03-17 16:27:19 +0100

    pierwszy commit pusty projekt  z mavenem

---
+++ src/test/java/Robot/JakubTest.java
@@ -0,0 +1,16 @@
+package Robot;
+
+import org.testng.annotations.Test;
+
+/**
+ * Created by jakub on 17.03.16.
+ */
+public class JakubTest {
+
+    @Test
+    public void test(){
+
+        assert true;
+    }
+
+}
