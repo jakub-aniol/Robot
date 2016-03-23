4af91f023c61a600776850817fe8e09add37d4b5
Jakub Aniol <ajakuba@wp.pl>
2016-03-23 21:18:44 +0100

    czyszczenie

--- src/main/java/Robot/Scanners/ScanRobotLegimi.java
+++ src/main/java/Robot/Scanners/ScanRobotLegimi.java
@@ -11,6 +11,7 @@ import java.util.ListIterator;
  */
 public class ScanRobotLegimi {
     final static Logger logger = Logger.getLogger(ScanRobotLegimi.class);
+
     BookStore bsLegimi;
 
     public ScanRobotLegimi(BookStore bookStore) {
